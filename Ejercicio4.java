d0a0f3cc0ecc655712490f1376e31999fe477dd7
PachecoASIR <jferradaspacheco@danielcastelao.org>
2022-11-23 17:57:53 +0100

    Commit7

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -3,13 +3,30 @@ import java.util.Scanner;
 public class Ejercicio4 {
     public static void main(String[] args) {
 
-        String n1;
+        double n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,resultado;
 
         Scanner leer_entrada = new Scanner(System.in);
-        System.out.println("Introduce una cadena de texto para mostrar cada caracter de manera individual: ");
-        n1=leer_entrada.nextLine();
-        for(int i=0; i<=n1.length()-1; i++) {
-            System.out.println(n1.charAt(i));
-        }   
+        System.out.println("Introduce un valor para poder calcular la media: ");
+        n1=leer_entrada.nextDouble();
+        System.out.println("Introduce un nuevo valor: ");
+        n2=leer_entrada.nextDouble();
+        System.out.println("Introduce un nuevo valor: ");
+        n3=leer_entrada.nextDouble();
+        System.out.println("Introduce un nuevo valor: ");
+        n4=leer_entrada.nextDouble();
+        System.out.println("Introduce un nuevo valor: ");
+        n5=leer_entrada.nextDouble();
+        System.out.println("Introduce un nuevo valor: ");
+        n6=leer_entrada.nextDouble();
+        System.out.println("Introduce un nuevo valor: ");
+        n7=leer_entrada.nextDouble();
+        System.out.println("Introduce un nuevo valor: ");
+        n8=leer_entrada.nextDouble();
+        System.out.println("Introduce un nuevo valor: ");
+        n9=leer_entrada.nextDouble();
+        System.out.println("Introduce un nuevo valor: ");
+        n10=leer_entrada.nextDouble();
+        resultado=(n1+n2+n3+n4+n5+n6+n7+n8+n9+n10)/10;
+        System.out.println("La media de los números introducidos es " + resultado);
     }
 }
